dfde074c58910f22115cdc862b656e9efa1c5d3c
Nikolay Yaryhin <nik.oroferov@gmail.com>
2021-06-02 21:03:10 +0300

    create

---
+++ index.js
@@ -0,0 +1,8 @@
+import './js/task-1.js';
+import './js/task-2.js';
+import './js/task-3.js';
+import './js/task-4.js';
+import './js/task-5.js';
+import './js/task-6.js';
+import './js/task-7.js';
+import './js/task-8.js';
