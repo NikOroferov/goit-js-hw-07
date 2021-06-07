001bc9de38863b24a3e18cd3ef1d932464576859
Nikolay Yaryhin <nik.oroferov@gmail.com>
2021-06-07 08:40:48 +0300

    a

--- js/task-6.js
+++ js/task-6.js
@@ -3,19 +3,14 @@
 // Если введено подходящее количество, то border инпута становится зеленым, если неправильное - красным.
 // Для добавления стилей, используй CSS-классы valid и invalid.
 
-
 const dataInput = document.querySelector('#validation-input');
 
 dataInput.addEventListener("blur", event => {
     const inputValue = event.currentTarget.value;
-    console.log(inputValue.length);
-    console.log(dataInput.dataset.length);
-    
-  if (inputValue.length <  dataInput.dataset.length) {
-    event.currentTarget.classList.add("invalid");
-  } 
 
-  else {
+  if (inputValue.length !== Number(dataInput.dataset.length)) {
+    event.currentTarget.classList.add("invalid");
+  } else {
     event.currentTarget.classList.remove("invalid");
     event.currentTarget.classList.add("valid");
   };
